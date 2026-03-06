80e6e358737663a54b22ff36e4bd9dde7ccf59de
Kaita Nakamura <kaita.nakamura0830@gmail.com>
2026-03-06 09:35:30 +0900

    chore
    Signed-off-by: Kaita Nakamura <kaita.nakamura0830@gmail.com>

---
+++ terraform/bedrock-agentcore-memory.tf
@@ -0,0 +1,16 @@
+#------------------------------------------------------------------------------
+# AgentCore Memory
+#------------------------------------------------------------------------------
+
+resource "aws_bedrockagentcore_memory" "main" {
+  name                  = replace(var.project_name, "-", "_")
+  description           = "Memory for ${var.project_name} agent"
+  event_expiry_duration = 30
+}
+
+resource "aws_bedrockagentcore_memory_strategy" "semantic" {
+  name       = "semantic_memory"
+  memory_id  = aws_bedrockagentcore_memory.main.id
+  type       = "SEMANTIC"
+  namespaces = ["/strategies/{memoryStrategyId}/actors/{actorId}/"]
+}
--- terraform/bedrock-agentcore-runtime.tf
+++ terraform/bedrock-agentcore-runtime.tf
@@ -145,20 +145,3 @@ resource "aws_bedrockagentcore_agent_runtime" "main" {
 
   depends_on = [aws_bedrockagentcore_memory_strategy.semantic]
 }
-
-#------------------------------------------------------------------------------
-# AgentCore Memory
-#------------------------------------------------------------------------------
-
-resource "aws_bedrockagentcore_memory" "main" {
-  name                  = replace(var.project_name, "-", "_")
-  description           = "Memory for ${var.project_name} agent"
-  event_expiry_duration = 30
-}
-
-resource "aws_bedrockagentcore_memory_strategy" "semantic" {
-  name       = "semantic_memory"
-  memory_id  = aws_bedrockagentcore_memory.main.id
-  type       = "SEMANTIC"
-  namespaces = ["/strategies/{memoryStrategyId}/actors/{actorId}/"]
-}
